8198db37d3d0a1ddab6e53245e2ab242c80e1f9b
eman6904 <neman6904@gmail.com>
2025-12-14 01:33:57 +0200

    new commit

--- app/src/main/java/com/example/firebaseapp/myPackages/data/remote/demoApi/Request.kt
+++
@@ -1,2 +0,0 @@
-package com.example.firebaseapp.myPackages.data.remote.demoApi
-
--- app/src/main/java/com/example/firebaseapp/myPackages/data/remote/demoApi/repo/NotificationRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.firebaseapp.myPackages.data.remote.demoApi.repo
-
-interface NotificationRepo {
-    suspend fun sendSharedNoteNotification(
-        title: String,
-        body: String,
-    ): Result<Unit>
-}
